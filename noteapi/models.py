f301d900578ce2f2bfcb88e2966173b3ec132ed2
Anirudhha <maneanirudha@gmail.com>
2023-08-13 17:50:40 +0530

    Created and migrated models and registed app in admin

---
+++ noteapi/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Notes(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    note = models.CharField(max_length=50,blank=False,default='')
+    notesesc = models.CharField(max_length=5000,blank=False,default='')
+
+
+    def __str__(self):
+        return self.note
+
+    class Meta:
+        ordering = ('id',)
